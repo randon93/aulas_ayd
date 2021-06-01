04158e0c222dca6fcfce54022e25a46c39d4268b
randon93 <jbgm93@gmail.com>
2021-06-01 09:32:08 -0500

    agregando SwaggerConfig.

--- src/main/java/com/ayd/aulas/controller/AulaController.java
+++ src/main/java/com/ayd/aulas/controller/AulaController.java
@@ -1,9 +1,15 @@
 package com.ayd.aulas.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aula")
 public class AulaController {
+
+    @GetMapping("/hola")
+    public void hola(){
+
+    }
 }
